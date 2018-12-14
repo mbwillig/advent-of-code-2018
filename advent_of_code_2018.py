ae181acdd2254c40e4f4790a8089d93dd4f118df
mbwillig <mbwillig@hotmail.com>
2018-12-14 12:20:03 +0100

    Add files via upload
    day 14

--- advent_of_code_2018.py
+++ advent_of_code_2018.py
@@ -657,8 +657,7 @@ def day13a():
                     "/l" :{UP,LEFT},
                     "\\l":{UP,RIGHT},
                     " ":set([]),
-                    "\n": set([]),
-                    "v":{UP,DOWN,RIGHT,LEFT}}
+                    "\n": set([]),}
 
     trainsymbol_to_dir = {
         "^" : UP,
@@ -766,8 +765,6 @@ def day13a():
                 return
 
 
-
-
 def day13b():
     from itertools import cycle
     LEFT = (0,-1) #drow, dcol
@@ -788,8 +785,7 @@ def day13b():
                     "/l" :{UP,LEFT},
                     "\\l":{UP,RIGHT},
                     " ":set([]),
-                    "\n": set([]),
-                    "v":{UP,DOWN,RIGHT,LEFT}}
+                    "\n": set([])}
 
     trainsymbol_to_dir = {
         "^" : UP,
@@ -901,4 +897,52 @@ def day13b():
             train.has_collided()
             trainlist = list(filter(lambda x: not x.colided, trainlist))
 
-day13b()
+def day14a():
+    scoreboard = [3,7]
+    elf_scoreboard_ix=[0,1]
+
+    def add_new_scores():
+        sum_scores = sum(scoreboard[x] for x in elf_scoreboard_ix)
+        new_recepies = [int(x) for x in list(str(sum_scores))]
+        scoreboard.extend(new_recepies)
+
+    def move_elfs():
+        return [(scoreboard[x]+1+x)%len(scoreboard) for x in elf_scoreboard_ix]
+
+    puzzle_input  = 47801
+    recepies_after = 10
+
+    while len(scoreboard)<(puzzle_input+recepies_after):
+        add_new_scores()
+        elf_scoreboard_ix = move_elfs()
+
+    print("".join([str(x) for x in scoreboard[puzzle_input:puzzle_input+recepies_after]]))
+
+def day14b():
+    scoreboard = [3,7]
+    elf_scoreboard_ix=[0,1]
+
+    def add_new_scores():
+        sum_scores = sum(scoreboard[x] for x in elf_scoreboard_ix)
+        new_recepies = [int(x) for x in list(str(sum_scores))]
+        scoreboard.extend(new_recepies)
+
+    def move_elfs():
+        return [(scoreboard[x]+1+x)%len(scoreboard) for x in elf_scoreboard_ix]
+
+    puzzle_input  ="047801"
+    len_input=len(str(puzzle_input))
+
+    while True:
+        add_new_scores()
+        elf_scoreboard_ix = move_elfs()
+        if "".join([str(x) for x in scoreboard[-len_input:]]) == puzzle_input:
+            print(len(scoreboard)-len_input)
+            break
+        if "".join([str(x) for x in scoreboard[(-len_input) -1:-1]]) == puzzle_input:
+            print((len(scoreboard)-1) - len_input)
+            break
+
+
+day14b()
+
